f9b1458b30da88498a49c16f02852975282740c2
Ten Cua Ban <ban@example.com>
2026-09-25 15:13:01 +0700

    Fix dbName

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -4,7 +4,9 @@ export const connectDB = async () => {
   try {
     console.log("Chuỗi kết nối Node.js đang đọc:", process.env.MONGODB_CONNECTIONSTRING);
     // @ts-ignore
-    await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING);
+    await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING, {
+      dbName: "AppTimMach" // <-- Nằm gọn trong ngoặc nhọn ở tham số thứ 2
+    });
     console.log("Liên kết CSDL thành công!");
   } catch (error) {
     console.log("Lỗi khi kết nối CSDL:", error);
